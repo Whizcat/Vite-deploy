da2ef48adb3fea165cea04b94d315b28887cf180
Whizcat <shaunthabo38@gmail.com>
2023-05-25 14:56:09 -1100

    secondd commit

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import handlebars from 'vite-plugin-handlebars';
+import data from "./data.json" assert {type: 'json'};
+
+
+export default {
+  plugins: [handlebars({
+    context : data
+  })]
+};
