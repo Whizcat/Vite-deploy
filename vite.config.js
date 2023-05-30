9b35877b30b72adff5c0c87d8c49f078817a8b32
Whizcat <shaunthabo38@gmail.com>
2023-05-29 16:00:37 -1100

    removed grid

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,23 @@
 import handlebars from 'vite-plugin-handlebars';
 import data from "./data.json" assert {type: 'json'};
+import { defineConfig } from 'vite';
 
 
-export default {
+export default defineConfig({
+  base : '/responsive_webpages/',
+  build: {
+      rollupOptions: {
+          input : {
+              main: 'index.html',
+              bootstrap: 'index-bootstrap.html',
+              materialize: 'index-materialize.html',
+          }
+      },
+  },
   plugins: [handlebars({
-    context : data
-  })]
-};
+    context : {
+      data
+    }
+})]
+  
+});
